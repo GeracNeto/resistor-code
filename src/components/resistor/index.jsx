5c1463e136fd3fe945f3c61e600f08a320b2f95e
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-05 18:58:07 -0300

    criado componente do resistor

---
+++ src/components/resistor/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export function Resistor(){
+    return(
+        <div className="master-container">
+            <div className="resistor">
+                <div className="box-one"></div>
+                <div className="box-two"></div>
+                <div className="box-three"></div>
+                <div className="box-four"></div>
+            </div>
+        </div>
+    );
+}
