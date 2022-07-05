c9fe9209d5a535615cbc0668f231815686d80038
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-05 20:30:04 -0300

    Criado useState par alterar a cor dos box, alterado lista de cores do ultimo box

--- src/components/resistor/index.jsx
+++ src/components/resistor/index.jsx
@@ -1,34 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './style.css';
 
 
 export function Resistor(){
 
-    return(
+    const [color, setColor] = useState("box-black");
 
+    return(
         <div className="master-container">
             <div className="resistor">
-                <div className="box"></div>
+                <div className='box'>{color}</div>
                 <div className="box"></div>
                 <div className="box"></div>
                 <div className="box"></div>
             </div>
+
             <div className="box-color">
-                <select>
-                    <option>Black</option>
-                    <option>Brown</option>
-                    <option>Red</option>
-                    <option>Orange</option>
-                    <option>Yellow</option>
-                    <option>Green</option>
-                    <option>Blue</option>
-                    <option>Pink</option>
-                    <option>Gray</option>
-                    <option>White</option>
-                </select>
-
-                <select>
-                <option>Black</option>
+                <select onChange={e => setColor(e.target.value)}>
+                    <option value="one-black">Black</option>
                     <option>Brown</option>
                     <option>Red</option>
                     <option>Orange</option>
@@ -64,6 +53,12 @@ export function Resistor(){
                     <option>Pink</option>
                     <option>Gray</option>
                     <option>White</option>
+                </select>
+
+                <select>
+                    <option>Gold</option>
+                    <option>Silver</option>
+                    <option>No-color</option>
                 </select>
             </div>
         </div>
