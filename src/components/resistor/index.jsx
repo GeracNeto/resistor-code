0690f6b7948333cf2dda5c9b28aba64860a75cbd
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-05 20:03:11 -0300

    Criado select para as cores do resistor e estilizado

--- src/components/resistor/index.jsx
+++ src/components/resistor/index.jsx
@@ -3,13 +3,68 @@ import './style.css';
 
 
 export function Resistor(){
+
     return(
+
         <div className="master-container">
             <div className="resistor">
                 <div className="box"></div>
                 <div className="box"></div>
                 <div className="box"></div>
                 <div className="box"></div>
+            </div>
+            <div className="box-color">
+                <select>
+                    <option>Black</option>
+                    <option>Brown</option>
+                    <option>Red</option>
+                    <option>Orange</option>
+                    <option>Yellow</option>
+                    <option>Green</option>
+                    <option>Blue</option>
+                    <option>Pink</option>
+                    <option>Gray</option>
+                    <option>White</option>
+                </select>
+
+                <select>
+                <option>Black</option>
+                    <option>Brown</option>
+                    <option>Red</option>
+                    <option>Orange</option>
+                    <option>Yellow</option>
+                    <option>Green</option>
+                    <option>Blue</option>
+                    <option>Pink</option>
+                    <option>Gray</option>
+                    <option>White</option>
+                </select>
+
+                <select>
+                    <option>Black</option>
+                    <option>Brown</option>
+                    <option>Red</option>
+                    <option>Orange</option>
+                    <option>Yellow</option>
+                    <option>Green</option>
+                    <option>Blue</option>
+                    <option>Pink</option>
+                    <option>Gray</option>
+                    <option>White</option>
+                </select>
+
+                <select>
+                    <option>Black</option>
+                    <option>Brown</option>
+                    <option>Red</option>
+                    <option>Orange</option>
+                    <option>Yellow</option>
+                    <option>Green</option>
+                    <option>Blue</option>
+                    <option>Pink</option>
+                    <option>Gray</option>
+                    <option>White</option>
+                </select>
             </div>
         </div>
     );
