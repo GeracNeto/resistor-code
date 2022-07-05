1b8350c0841589695fbe2c5506b7996711a63b8f
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-05 18:59:06 -0300

    Deletado componente padrão do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Resistor } from './components/resistor'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <Resistor />
   </React.StrictMode>
 )
