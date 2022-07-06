839c3b4d402a7021c300b0f98ae93e65d666fa55
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-06 19:39:33 -0300

    Criado footer do projeto

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export function Footer(){
+    return(
+        <footer>
+            <h3>Desenvolvido por Geraldo Pereira da Costa Neto</h3>
+            <div className='sociais'>
+                <a href='https://github.com/GeracNeto' target='_blank'><img src='/placa-do-github.png' id='git-img'></img></a>
+                <a href='https://www.linkedin.com/in/geraldo-costa-5568a8152/' target='_blank'><img src='/linkedin.png'></img></a>
+            </div>
+        </footer>
+    );
+}
