301054dcab518b3771d97e57d5930d0ea14acb03
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-06 20:01:34 -0300

    Criado header do projeto

---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+export function Header(){
+    return(
+        <header>
+            <h1>identificador de cores</h1>
+            <h2>resistor 4 bandas</h2>
+        </header>
+    )
+}
