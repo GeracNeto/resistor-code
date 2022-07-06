0b1a2a5812d3619c430bc398eec7e8ad7fcd8de7
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-06 19:39:54 -0300

    Importado footer para o main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Footer } from './components/footer'
 import { Resistor } from './components/resistor'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <h1>header here</h1>
     <Resistor />
+    <Footer />
   </React.StrictMode>
 )
