e5191b291bdcd26dcb18077b7cc885bbbef9b09a
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-07 10:49:13 -0300

    Atualizado caminho do arquivo App.jsx

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
-import { Header } from "./components/header";
-import { Resistor } from "./components/resistor";
-import { Footer } from './components/footer';
+import { Header } from "../components/header";
+import { Resistor } from "../components/resistor";
+import { Footer } from '../components/footer';
+
+import './style.css'
 
 export function App(){
 
     const [isToggled, setIsToggled] = useState(false);
 
     return(
-        <>
+        <div>
             <Header />
             <Resistor />
             <button onClick={() => setIsToggled(!isToggled)}>Obs</button>
             {isToggled && <Footer />}
-        </>
+        </div>
     );
 }
