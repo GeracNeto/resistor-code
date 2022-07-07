baa5046080b896305ebeefa1fd86f9dcb37a5f25
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-07 10:12:18 -0300

    Alterado main para importar apenas o componente App

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Footer } from './components/footer'
-import { Header } from './components/header'
-import { Resistor } from './components/resistor'
+import { App } from './App'
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
-    <Resistor />
-    <Footer />
+    <App />  
   </React.StrictMode>
 )
